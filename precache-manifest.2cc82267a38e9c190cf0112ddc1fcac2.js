5eb811cfad85ade1d2d30829319ae69d94d5e62f
DariaKat <51202251+DariaKat@users.noreply.github.com>
2020-07-21 19:53:09 +0300

    Updates

---
+++ precache-manifest.2cc82267a38e9c190cf0112ddc1fcac2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e807cc49e75db847edd1c13f9144fadf",
+    "url": "/Front-app/index.html"
+  },
+  {
+    "revision": "9c37db4cfadee85e90bf",
+    "url": "/Front-app/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "1641340e8038876c378c",
+    "url": "/Front-app/static/css/main.11239e9d.chunk.css"
+  },
+  {
+    "revision": "9c37db4cfadee85e90bf",
+    "url": "/Front-app/static/js/2.65be486a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Front-app/static/js/2.65be486a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1641340e8038876c378c",
+    "url": "/Front-app/static/js/main.497fa6bd.chunk.js"
+  },
+  {
+    "revision": "65e67ba5fed7d7978d39",
+    "url": "/Front-app/static/js/runtime-main.71ce6c91.js"
+  }
+]);
